e599b8cd2cbfab23ef3fa871ef0497c457b7fa78
Tianer Zhou <zhoutianer@huawei.com>
2024-03-30 14:30:48 +0800

    add dialog
    Signed-off-by: Tianer Zhou <zhoutianer@huawei.com>

--- app/dialog.tsx
+++ app/dialog.tsx
@@ -1,3 +1,4 @@
+'use client'
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
@@ -7,6 +8,20 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const themeUITheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1898db",
+    },
+    background: {
+      default: "#07141a",
+    }
+  },
+});
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   '& .MuiDialogContent-root': {
@@ -24,48 +39,45 @@ type DialogProps = {
 
 export default function AccountSettingDialog({ displayDialog, isOpen }: DialogProps) {
   return (
-    <React.Fragment>
-      <BootstrapDialog
-        onClose={() => displayDialog(false)}
-        aria-labelledby="customized-dialog-title"
-        open={isOpen}
-      >
-        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
-          Modal title
-        </DialogTitle>
-        <IconButton
-          aria-label="close"
-          onClick={() => displayDialog(false)}
-          sx={{
-            position: 'absolute',
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
-          }}
+    <ThemeProvider theme={themeUITheme}>
+      <React.Fragment>
+        <BootstrapDialog
+          onClose={() => displayDialog(false)}
+          aria-labelledby="customized-dialog-title"
+          open={isOpen}
         >
-        </IconButton>
-        <DialogContent dividers>
-          <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </Typography>
-          <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-          </Typography>
-          <Typography gutterBottom>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
-            magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
-            ullamcorper nulla non metus auctor fringilla.
-          </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={() => displayDialog(false)}>
-            Save changes
-          </Button>
-        </DialogActions>
-      </BootstrapDialog>
-    </React.Fragment>
+          <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+            Modal title
+          </DialogTitle>
+          <IconButton
+            aria-label="close"
+            onClick={() => displayDialog(false)}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+          </IconButton>
+          <DialogContent dividers>
+            <Typography gutterBottom>
+              Click the button below to log out of your profile.
+            </Typography>
+            <Typography gutterBottom>
+              Click the button below to download all data that is associated with your profile. This data includes all conversations stored in Grok as well as all prompts stored in the PromptIDE.
+            </Typography>
+            <Typography gutterBottom>
+              Clicking the button below erases all data that is associated with your profile. This data includes everything stored in Grok and the PromptIDE. Note that this action is immediate and irreversible. Once the profile has been deleted, there is no way to recover any of the data.
+            </Typography>
+          </DialogContent>
+          <DialogActions>
+            <Button autoFocus onClick={() => displayDialog(false)}>
+              Save changes
+            </Button>
+          </DialogActions>
+        </BootstrapDialog>
+      </React.Fragment>
+    </ThemeProvider>
   );
 }
