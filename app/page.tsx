7b1526a7da06a87add48b1dc3a69db89ade93ae1
Tianer Zhou <zhoutianer@huawei.com>
2024-03-30 11:05:31 +0800

    add grok logo
    Signed-off-by: Tianer Zhou <zhoutianer@huawei.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,27 @@
-import Image from "next/image";
+'use client'
+
 import './animation.css'
 import './fonts.css'
+import LoginPage from "./login";
+import { useState } from "react";
 
 export default function Home() {
+  const [dialogOpen, setDialog] = useState(false)
   const spanStyle = "relative block text-white text-3xl text-center p-1"
   const absoluteTop = "absolute top-0 left-0"
+
   return (
-    <main className="bg-black flex h-screen w-screen flex-row items-center justify-stretch box-border">
-      <div className="hidden md:flex h-screen flex-1 flex-col place-content-center">
+    <main className="bg-black flex w-screen flex-row box-border">
+      <div className="relative hidden md:flex flex-1 flex-col place-content-center p-20">
         {/* Background */}
-        <div className="absolute top-0 left-0 right-0 bottom-0 z-index-0 w-1/2 h-full">
+        <div className="z-index-0">
 
           <div className={`${absoluteTop} w-full opacity-20 bg-large-star`}></div>
-          <div className={`${absoluteTop} w-full h-full opacity-5 bg-small-star`}></div>
+          <div className={`${absoluteTop} w-full opacity-5 bg-small-star`}></div>
           <div className={`${absoluteTop} gradient w-full h-full z-index-2`}></div>
         </div>
         {/* Title */}
-        <div className="title relative flex-wrap flex place-content-center p-20">
+        <div className="title relative flex-wrap flex place-content-center">
           <span className={spanStyle}>Conversational</span>
           <span className={spanStyle}>AI</span>
           <span className={spanStyle}>for</span>
@@ -24,10 +29,16 @@ export default function Home() {
           <span className={spanStyle}>the</span>
           <span className={spanStyle}>Universe</span>
         </div>
+        <div className="absolute top-6 left-6 w-full">
+          <img src="/grok.svg" alt="grok logo" className="w-32 h-11" />
+        </div>
       </div>
-      <div className="h-screen flex-1 flex-col flex items-center justify-center">
-        <p className="p-20 text-2xl text-center">Thank you for applying to our early access program. The program is currently limited to <a href="x.com">X Premium+</a> subscribers.</p>
-      </div>
+      <LoginPage openDialog={setDialog.bind(null, true)} />
+      {dialogOpen && (
+        <div>
+          <p>Opened</p>
+        </div>
+      )}
     </main>
   );
 }
